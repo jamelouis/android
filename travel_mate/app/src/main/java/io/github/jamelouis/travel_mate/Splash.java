2194b5c7f1cc48ee682c678f467f7e7a777f13c4
jamelouis <jamelouis2015@gmail.com>
2018-07-04 21:35:04 +0800

    splash screen

---
+++ travel_mate/app/src/main/java/io/github/jamelouis/travel_mate/Splash.java
@@ -0,0 +1,41 @@
+package io.github.jamelouis.travel_mate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+import com.eftimoff.androipathview.PathView;
+
+import java.util.Objects;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        final PathView pathView = findViewById(R.id.pathView);
+        pathView.getPathAnimator()
+                .delay(1000)
+                .duration(1000)
+                .interpolator(new AccelerateDecelerateInterpolator())
+                .start();
+
+        pathView.useNaturalColors();
+        pathView.setFillAfter(true);
+
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(Splash.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }, 2000);
+    }
+}
