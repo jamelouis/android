ffec2bc9ddc5b797390fb5a34751aea5d1e35c68
jamelouis <jamelouis2015@gmail.com>
2018-07-25 10:43:09 +0800

    add adb demo

---
+++ adb_demo/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,13 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT jstring
+
+JNICALL
+Java_io_github_jamelouis_adb_1demo_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
